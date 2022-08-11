7e26e9156b8a9ae18fb50b373c75331d8236ed0e
Israelote123 <joseprinsisrael@gmail.com>
2022-08-10 20:45:38 -0500

    mensaje de error

--- app.js
+++ app.js
@@ -1,122 +1,159 @@
 const encryptKeys = {
-   a:"ai",
-   e:"enter",
-   i:"imes",
-   o:"ober",
-   u:"ufat",
+  a: "ai",
+  e: "enter",
+  i: "imes",
+  o: "ober",
+  u: "ufat",
 };
 
 const decryptKeys = {
-    ai:"a",
-    enter:"e",
-    imes:"i",
-    ober:"o",
-    ufat:"u",
-}
+  ai: "a",
+  enter: "e",
+  imes: "i",
+  ober: "o",
+  ufat: "u",
+};
 
 //Boton para guardar en el portapapeles
 let copy = () => {
-    let resultData;
+  let resultData;
 
-    if(screen.width < 769){
-        resultData = document.getElementById('processResult').value;
-    } else {
-        resultData = document.getElementById('textResult').textContent;
-    }
+  if (screen.width < 769) {
+    resultData = document.getElementById("processResult").value;
+  } else {
+    resultData = document.getElementById("textResult").textContent;
+  }
 
-    navigator.clipboard.writeText(resultData)
+  navigator.clipboard
+    .writeText(resultData)
     .then(() => {
-        console.log('Texto copiado')
-        if(screen.width < 769){
-            Alert.ok();
-        }
+      console.log("Texto copiado");
+      if (screen.width < 769) {
+        Alert.ok();
+      }
     })
-    .catch(err => {
-        console.log('Hubo un error ', err);
-    })
-
-
-}
+    .catch((err) => {
+      console.log("Hubo un error ", err);
+    });
+};
 
 //Manejo de pop up (tablets a telefonos)
 
 var Alert = new CustomAlert();
 
-function CustomAlert(){
-    this.render = function(){
-        let popUpBox = document.getElementById('popUpBox');
-        let back = document.getElementById('popUpOverlay');
-        back.style.display = "block";
-        popUpBox.style.display = "block";
-        document.getElementById('closeModal').innerHTML = `<button type="button" class="btn btn-outline-success" onclick="copy();"><i class="far fa-copy"></i></button>`;
-    }
+function CustomAlert() {
+  this.render = function () {
+    let popUpBox = document.getElementById("popUpBox");
+    let back = document.getElementById("popUpOverlay");
+    back.style.display = "block";
+    popUpBox.style.display = "block";
+    document.getElementById(
+      "closeModal"
+    ).innerHTML = `<button type="button" class="btn btn-outline-success" onclick="copy();"><i class="far fa-copy"></i></button>`;
+  };
 
-    this.ok = function(){
-        document.getElementById('popUpBox').style.display = `none`;
-        document.getElementById('popUpOverlay').style.display = `none`;
-    }
+  this.ok = function () {
+    document.getElementById("popUpBox").style.display = `none`;
+    document.getElementById("popUpOverlay").style.display = `none`;
+  };
 }
 
 //cambia el cuadro en el cual puede escribir el usuario
-if(window.screen.width <= 768){
-    document.querySelector('.user-entry-container').contentEditable = true;
+if (window.screen.width <= 768) {
+  document.querySelector(".user-entry-container").contentEditable = true;
 }
 
 //Guardado real del texto ingresado
 let realSave = (userText) => {
-    document.querySelector('[name="user_entry"]').value = userText.innerText;
-}
+  document.querySelector('[name="user_entry"]').value = userText.innerText;
+};
 
 //Obtiene el texto que ingreso el usuario
 let obtainData = () => {
-    return document.querySelector('[name="user_entry"]').value
-}
+  let obtainedData = document.querySelector('[name="user_entry"]').value;
+  if (obtainedData == "") {
+    return false;
+  }
+  return obtainedData;
+};
 
 //Encripta la informacion
 let encrypt = (matched) => {
-    return encryptKeys[matched];
-}
+  return encryptKeys[matched];
+};
 
 let encryptUserEntry = (data) => {
-    return data.replace(/a|e|i|o|u/g, encrypt);
-}
+  return data.replace(/a|e|i|o|u/g, encrypt);
+};
 
 //Desencripta la informacion
 let decrypt = (matched) => {
-    return decryptKeys[matched];
-}
+  return decryptKeys[matched];
+};
 
 let decryptUserEntry = (data) => {
-    return data.replace(/ai|enter|imes|ober|ufat/g, decrypt);
-}
+  return data.replace(/ai|enter|imes|ober|ufat/g, decrypt);
+};
 
 //Esconde la imagen de resultados no encontrados
 let hideImage = () => {
-    document.getElementById('anyResult').style.display = `none`
-}
+  document.getElementById("anyResult").style.display = `none`;
+};
 
 //Muestra la información encriptada
 let showEncrypted = () => {
-    let dataEncrypted = encryptUserEntry(obtainData());
-    if(screen.width < 769){
-        Alert.render('You look very pretty today.');
-        document.getElementById('processResult').value = `${dataEncrypted}`;
-    } else {
-        document.getElementById('anyResult').style.display = `none`;
-        document.getElementById('copy').style.display = `block`;
-        document.getElementById('textResult').textContent = `${dataEncrypted}`;
+  let dataOfUser = obtainData();
+  if (!dataOfUser) {
+    document.getElementById("noText").style.display = `block`;
+    if (screen.width > 768) {
+      document.getElementById("anyResult").style.display = `block`;
+      document.getElementById("textResult").style.display = `none`;
+      document.getElementById("floatingTextarea").style.height = `500px`;
     }
-}
+  } else {
+    let dataEncrypted = encryptUserEntry(dataOfUser);
+    document.getElementById("noText").style.display = `none`;
+    document.getElementById("textResult").style.display = `block`;
+    document.getElementById("floatingTextarea").style.height = `600px`;
+    if (screen.width > 1280) {
+      document.getElementById("floatingTextarea").style.height = `700px`;
+    }
+    if (screen.width < 769) {
+      Alert.render("You look very pretty today.");
+      document.getElementById("processResult").value = `${dataEncrypted}`;
+    } else {
+      document.getElementById("anyResult").style.display = `none`;
+      document.getElementById("copy").style.display = `block`;
+      document.getElementById("textResult").textContent = `${dataEncrypted}`;
+    }
+  }
+};
 
 //Muestra la información desencriptada
 let showDecrypted = () => {
-    let dataDecrypted = decryptUserEntry(obtainData());
-    if(screen.width < 769){
-        Alert.render('You look very pretty today.');
-        document.getElementById('processResult').value = `${dataDecrypted}`;
-    } else {
-        document.getElementById('anyResult').style.display = `none`;
-        document.getElementById('copy').style.display = `block`;
-        document.getElementById('textResult').textContent = `${dataDecrypted}`;
+  let dataOfUser = obtainData();
+  if (!dataOfUser) {
+    document.getElementById("noText").style.display = `block`;
+    if (screen.width > 768) {
+      document.getElementById("anyResult").style.display = `block`;
+      document.getElementById("textResult").style.display = `none`;
+      document.getElementById("floatingTextarea").style.height = `500px`;
     }
-}
+  } else {
+    let dataDecrypted = decryptUserEntry(dataOfUser);
+    document.getElementById("noText").style.display = `none`;
+    document.getElementById("textResult").style.display = `block`;
+    document.getElementById("floatingTextarea").style.height = `600px`;
+    if (screen.width > 1280) {
+      document.getElementById("floatingTextarea").style.height = `700px`;
+    }
+    if (screen.width < 769) {
+      Alert.render("You look very pretty today.");
+      document.getElementById("processResult").value = `${dataDecrypted}`;
+    } else {
+      document.getElementById("anyResult").style.display = `none`;
+      document.getElementById("copy").style.display = `block`;
+      document.getElementById("textResult").textContent = `${dataDecrypted}`;
+    }
+  }
+};
